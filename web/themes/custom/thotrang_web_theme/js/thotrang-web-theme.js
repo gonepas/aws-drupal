9adf5dd1b3b09a6459934c098e6fba3151b223f7
gonepas <xuantho995@gmail.com>
2023-05-11 18:00:02 +0700

    new UI for landing page

--- web/themes/custom/thotrang_web_theme/js/thotrang-web-theme.js
+++ web/themes/custom/thotrang_web_theme/js/thotrang-web-theme.js
@@ -2,7 +2,7 @@
  * @file
  * ThoTrang Web Theme behaviors.
  */
-(function (Drupal) {
+(function (Drupal, $) {
 
   'use strict';
 
@@ -14,4 +14,43 @@
     }
   };
 
-} (Drupal));
+  Drupal.behaviors.frontPillar = {
+    attach: function (context, settings) {
+
+      var min_w = 300;
+      var vid_w_orig;
+      var vid_h_orig;
+
+      $(function() {
+
+        vid_w_orig = parseInt($('video').attr('width'));
+        vid_h_orig = parseInt($('video').attr('height'));
+
+        $(window).resize(function () { fitVideo(); });
+        $(window).trigger('resize');
+
+      });
+
+      function fitVideo() {
+
+        $('#video-viewport').width($('.fullsize-video-bg').width());
+        $('#video-viewport').height($('.fullsize-video-bg').height());
+
+        var scale_h = $('.fullsize-video-bg').width() / vid_w_orig;
+        var scale_v = $('.fullsize-video-bg').height() / vid_h_orig;
+        var scale = scale_h > scale_v ? scale_h : scale_v;
+
+        if (scale * vid_w_orig < min_w) {scale = min_w / vid_w_orig;};
+
+        $('video').width(scale * vid_w_orig);
+        $('video').height(scale * vid_h_orig);
+
+        $('#video-viewport').scrollLeft(($('video').width() - $('.fullsize-video-bg').width()) / 2);
+        $('#video-viewport').scrollTop(($('video').height() - $('.fullsize-video-bg').height()) / 2);
+
+      };
+
+    }
+  };
+
+} (Drupal, jQuery));
